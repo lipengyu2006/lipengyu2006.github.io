bb63e4bd1303076c52b0c9a39bf1981bd55d9ef7
lipengyu2006 <lipengyu2006@gmail.com>
2017-11-16 10:22:13 +0800

    onionify version

--- js/src/Item.js
+++ js/src/Item.js
@@ -1,34 +1,28 @@
 import xs from 'xstream';
-import {div, span, a, input} from '@cycle/dom';
-import {table, thead, tbody, th, tr, td} from '@cycle/dom';
+import { div, span, a, input } from '@cycle/dom';
+import { table, thead, tbody, th, tr, td } from '@cycle/dom';
 
 function intent(domSource) {
-  return domSource.select('.is-delete').events('click')
-    .mapTo({type: 'REMOVE'})
+  return domSource
+    .select('.is-delete')
+    .events('click')
+    .mapTo({ type: 'REMOVE' })
 }
 
-function model(props$, actions$) {
+function model(actions$) {
+  const deleteReducer$ = actions$
+    .filter(action => action.type === 'REMOVE')
+    .mapTo(function removeReducer(prevState) {
+      return undefined;
+    })
 
-  return props$.map(props => {
-      return {
-        id: props.id
-      };
-    });
-
-  //return props$.map(props => {
-  //  return action$.startWith(null)
-  //    .map(value => {
-  //      return {
-  //        id: props.id
-  //      };
-  //    })
-  //}).flatten();
+  return deleteReducer$;
 }
 
 function view(state$) {
-  return state$.map(({id}) => {
-    return tr([
-      th(`${id}`),
+  return state$.map((state) => 
+    tr([
+      th(`${state.id}`),
       td(div('.tags.has-addons', [
         span('.tag.is-primary', '254'),
         span('.tag', '①')
@@ -43,39 +37,19 @@ function view(state$) {
       td(input('.input.is-small', {
         attrs: {type: 'text'}
       }))
-    ]);
-  });
+    ])
+  );
 }
 
 function Item(sources) {
+  const state$ = sources.onion.state$;
   const action$ = intent(sources.DOM);
-  const state$ = model(sources.Props, action$);
+  const reducer$ = model(action$);
   const vtree$ = view(state$);
   return {
     DOM: vtree$,
-    Remove: action$.filter(a => a.type === 'REMOVE')
+    onion: reducer$
   };
-}
-
-
-function makeListTr(props) {
-  return tr([
-    th(`${props.id}`),
-    td(div('.tags.has-addons', [
-      span('.tag.is-primary', '254'),
-      span('.tag', 'б┘')
-    ])),
-    td('17:30:15'),
-    td('17:31:15'),
-    td('1ио30├в'),
-    td(div('.tags.has-addons', [
-      a('.tag.is-light', 'Stop'),
-      a('.tag.is-delete'),
-    ])),
-    td(input('.input.is-small', {
-      attrs: {type: 'text'}
-    }))
-  ]);
 }
 
 export default Item;
--- js/src/app.js
+++ js/src/app.js
@@ -1,112 +1,86 @@
 import xs from 'xstream';
-import isolate from '@cycle/isolate'
-import {run} from '@cycle/run';
-import {div, span, a, input, makeDOMDriver} from '@cycle/dom';
-import {table, thead, tbody, th, tr, td} from '@cycle/dom';
-import {abbr} from '@cycle/dom';
+import isolate from '@cycle/isolate';
+import { run } from '@cycle/run';
+import onionify, { makeCollection } from 'cycle-onionify';
+import { div, span, a, input, makeDOMDriver } from '@cycle/dom';
+import { table, thead, tbody, th, tr, td } from '@cycle/dom';
+import { abbr, button, p } from '@cycle/dom';
 import Item from './Item';
 
-function intent(domSource, itemRemove$) {
-  return xs.merge(
-    domSource.select('.add-one-item').events('click')
-      .mapTo({type: 'ADD_ITEM', payload: 1}),
+function intent(domSource) {
+  const add$ = domSource
+    .select('.add-one-item')
+    .events('click')
+    .mapTo({type: 'ADD_ITEM', payload: 1});
 
-    itemRemove$.map(id => ({type: 'REMOVE_ITEM', payload: id}))
-  );
+  return add$;
 }
 
-function model(action$, itemFn) {
-  const addItemReducer$ = action$
-    .filter(a => a.type === 'ADD_ITEM')
-    .map(action => {
-      const amount = action.payload;
-      let newItems = [];
-      for (let i=0; i<amount; i++) {
-        newItems.push(createNewItem());
-      }
-      return function addItemReducer(listItems) {
-        return listItems.concat(newItems);
-      };
-    });
-
-  const removeItemReducer$ = action$
-    .filter(a => a.type === 'REMOVE_ITEM')
-    .map(action => function removeItemReducer(listItems) {
-      return listItems.filter(item => item.id !== action.payload)
-    });
-
+function model(action$) {
   let mutableLastId = 0;
-  function createNewItem() {
-    const id = mutableLastId++;
-    const sinks = itemFn(id);
+  const initReducer$ = xs.of((prev) => {
     return {
-      id,
-      DOM: sinks.DOM,
-      Remove: sinks.Remove
+      list: []
     };
-  }
+  });
+  const addReducer$ = action$
+    .filter(action => action.type === 'ADD_ITEM')
+    .map((action) => (prevState) => {
+      return {
+        list: prevState.list.concat({ id: mutableLastId++ }) 
+      }
+    });
 
-  return xs.merge(addItemReducer$, removeItemReducer$).fold((listItems, reducer) => reducer(listItems), []);
+  return xs.merge(initReducer$, addReducer$);
 }
 
-function view(items$) {
-  return items$.map(items => {
-    const itemVNodeStreamsByKey = items.map(item => {
-      return item.DOM.map(vnode => {
-        vnode.key = item.id;
-        return vnode;
-      })
-    });
-    return xs.combine(...itemVNodeStreamsByKey)
-      .map(vnodes => makeSkeleton(vnodes));
-  }).flatten();
+function view(listVNode$) {
+  return listVNode$.map(listVnode =>
+    div('.wrap', [
+      a('.button.add-one-item', 'Add'),
+      table('.table', [
+        thead(tr([
+          th(abbr({attrs: {title: 'Position'}}, 'Pos')),
+          th('Section'),
+          th('Start time'),
+          th('End time'),
+          th('Duration'),
+          th('Operate'),
+          th('Content')
+        ])),
+        listVnode
+      ])
+    ])
+  )
 }
 
 function main(sources) {
-  const proxyItemRemove$ = xs.create();
-  const itemWrapper = makeItemWrapper(sources);
-  const action$ = intent(sources.DOM, proxyItemRemove$);
-  const items$ = model(action$, itemWrapper);
-  const itemRemove$ = items$
-    .map(items => {
-      const itemRemoveStreams = items.map(item => item.Remove);
-      return xs.merge(...itemRemoveStreams);
-    }).flatten()
-  proxyItemRemove$.imitate(itemRemove$);
-  const vtree$ = view(items$);
+  const List = makeCollection({
+    item: Item,
+    itemKey: (s, k) => s.id,
+    itemScope: (key) => key,
+    collectSinks: (instances) => {
+      return {
+        DOM: instances.pickCombine('DOM')
+          .map((itemVNodes) => tbody(itemVNodes)),
+        onion: instances.pickMerge('onion')
+      };
+    }
+  });
+  const listSinks = isolate(List, 'list')(sources);
+  const action$ = intent(sources.DOM);
+  const parentReducer$ = model(action$);
+  const listReducer$ = listSinks.onion;
+  const reducer$ = xs.merge(parentReducer$, listReducer$);
+  const vdom$ = view(listSinks.DOM);
+
   return {
-    DOM: vtree$
+    DOM: vdom$,
+    onion: reducer$
   };
 }
 
-function makeItemWrapper(sources) {
-  return function itemWrapper(id) {
-    const item = isolate(Item)({...sources, Props: xs.of({id})});
-    return {
-      DOM: item.DOM,
-      Remove: item.Remove.mapTo(id)
-    }
-  }
-}
-
-function makeSkeleton(listItems) {
-  return div('.wrap', [
-    a('.button.add-one-item', 'Add'),
-    table('.table', [
-      thead(tr([
-        th(abbr({attrs: {title: 'Position'}}, 'Pos')),
-        th('Section'),
-        th('Start time'),
-        th('End time'),
-        th('Duration'),
-        th('Operate'),
-        th('Content')
-      ])),
-      tbody(listItems)
-    ])
-  ]);
-}
-
-run(main, {
+var wrapperMain = onionify(main);
+run(wrapperMain, {
   DOM: makeDOMDriver('#timerecord')
 });
